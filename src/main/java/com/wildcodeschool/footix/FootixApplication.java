bab87e927d9b9e755b7894746a3862070b6593a2
Alanrl22 <alan.raoul22@gmail.com>
2020-07-22 11:36:28 +0200

    initial commit

---
+++ src/main/java/com/wildcodeschool/footix/FootixApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.footix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootixApplication.class, args);
+	}
+
+}
