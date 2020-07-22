b9e7837991db439d63f9ed14bdf664c735a2c3c1
Alanrl22 <alan.raoul22@gmail.com>
2020-07-22 12:53:33 +0200

    created Autowired

--- src/main/java/com/wildcodeschool/footix/controller/HomePageController.java
+++ src/main/java/com/wildcodeschool/footix/controller/HomePageController.java
@@ -1,9 +1,20 @@
 package com.wildcodeschool.footix.controller;
 
+import com.wildcodeschool.footix.repository.ClubRepository;
+import com.wildcodeschool.footix.repository.PlayerRepository;
+import com.wildcodeschool.footix.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomePageController {
 
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @Autowired
+    private ClubRepository clubRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 }
